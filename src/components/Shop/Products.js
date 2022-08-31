7d2501000fab092144c283e2c4848b899caabb2c
RushikeshGandhmal <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-08-31 20:45:46 +0530

    Update Products.js

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -3,7 +3,7 @@ import classes from './Products.module.css';
 
 const DUMMY_PRODUCTS = [
   {
-    id: 'p1',
+    id: 'p2',
     price: 6,
     title: 'My First Book',
     description: 'The first book I ever wrote',
